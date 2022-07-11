393750a51437a4b35a9a19dcefeeab4fcaf82650
jui2542123 <jui2542123@hotmail.com>
2022-07-11 23:52:11 +0700

    fetch api

--- question2/src/App.js
+++ question2/src/App.js
@@ -1,7 +1,44 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [names, setName] = useState([]);
+  // let query = searchInput;
+  // if (!query) {
+  //   query = "";
+  // }
+
+  const datas = async () => {
+    const response = await fetch("https://api.publicapis.org/categories");
+    // const check = await response.json();
+    // console.log(check);
+    setName(await response.json());
+  };
+
+  useEffect(() => {
+    datas();
+  }, []);
+
+  return (
+    <div className="App">
+      <form className="filter">
+        <input
+          type="text"
+          // value={filterInput}
+          // onChange={setSearchInput(e.target.value)}
+        />
+      </form>
+      <div className="table">
+        <table>
+          {names.categories.map((name) => (
+            <tr>
+              <td>{name}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default App;
